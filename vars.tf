0af537ffa95bce68eab2cae282432c3e398db0aa
Sandeep0045 <38896678+Sandeep0045@users.noreply.github.com>
2020-09-25 10:04:19 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,10 +1,6 @@
-variable "okta_group_name"{
-  default = "prod"
-  }
+variable "okta_group_name"{}
 
-variable "okta_group_description"{
-  default = "prod server"
-  }
+variable "okta_group_description"{}
 
 variable "okta_api_token" {
   default = "00WDvVIr3Y6Wg3o08fG_Pxstf6qNIGgcKQEOVHw5sy"
